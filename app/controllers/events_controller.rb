90fe9b7ca971467d37d14323aab1c75fca8ddc1d
MclPio <113801014+MclPio@users.noreply.github.com>
2023-11-14 14:02:23 -0500

    Add #create method for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,12 +6,24 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
   end
+  
   def new
     @event = Event.new
   end
 
   def create
-    
+    @event = Event.build_event(event_params)
+
+    if @event.save
+      redirect_to @event
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
-  
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :location, :date_start, :date_end)
+  end
 end
