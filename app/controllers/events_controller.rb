7226645cb568a9f69d64fc80bec6e27b89b8258f
MclPio <113801014+MclPio@users.noreply.github.com>
2023-11-14 13:53:53 -0500

    Add event list to index page of events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,17 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.all
   end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+  def new
+    @event = Event.new
+  end
+
+  def create
+    
+  end
+  
 end
