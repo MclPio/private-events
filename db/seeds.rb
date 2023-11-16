68dfd2a0d1cb45327e7d7bac6030f33d3afd7cff
MclPio <113801014+MclPio@users.noreply.github.com>
2023-11-16 18:39:55 -0500

    Build db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+bob = User.new
+bob.email = 'bob@world.co'
+bob.username = 'bob'
+bob.password = '123456'
+bob.password_confirmation = '123456'
+bob.save!
+
+event = User.find_by(username: 'bob').events.build(date_start: '2024-11-11 16', date_end: '2024-11-11 19', location: 'Leafs Coffee', description: 'bla bla bla bla bla words words words.', title: 'Art Meet')
+event.save!
+
+joe = User.new
+joe.email = 'joe@world.co'
+joe.username = 'joe'
+joe.password = '123456'
+joe.password_confirmation = '123456'
+joe.save!
+
+event = User.find_by(username: 'joe').events.build(date_start: '2024-11-11 16', date_end: '2024-11-11 19', location: 'Leafs Coffee', description: 'bla bla bla bla bla words words words.', title: 'Tech Meet')
+event.save!
