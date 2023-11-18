a4f8c7dbfe141be709d8a0a95e9aae236df18ab2
MclPio <113801014+MclPio@users.noreply.github.com>
2023-11-18 14:19:38 -0500

    implement db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,36 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-bob = User.new
-bob.email = 'bob@world.co'
-bob.username = 'bob'
-bob.password = '123456'
-bob.password_confirmation = '123456'
-bob.save!
 
-event = User.find_by(username: 'bob').events.build(date_start: '2024-11-11 16', date_end: '2024-11-11 19', location: 'Leafs Coffee', description: 'bla bla bla bla bla words words words.', title: 'Art Meet')
-event.save!
 
-joe = User.new
-joe.email = 'joe@world.co'
-joe.username = 'joe'
-joe.password = '123456'
-joe.password_confirmation = '123456'
-joe.save!
+event = User.find_by(username: 'joe').
+events.build(date_start: '2024-11-11 16', date_end: '2024-11-11 19', 
+location: 'Leafs Coffee', description: 'bla bla bla bla bla words words words.', 
+title: 'Tech Meet')
+# event.save!
 
-event = User.find_by(username: 'joe').events.build(date_start: '2024-11-11 16', date_end: '2024-11-11 19', location: 'Leafs Coffee', description: 'bla bla bla bla bla words words words.', title: 'Tech Meet')
-event.save!
+def user_gen
+  for i in 1..100 do
+    user_name = "user#{i}"
+    User.create(username: user_name, 
+                email: "#{user_name}@world.co",
+                password: "123456",
+                password_confirmation: "123456")
+  end
+end
+
+def event_gen()
+  for i in 1.20 do
+    event_name = "event #{i}"
+    date_start = "2024-01-#{i} 16"
+    date_end = "2024-01-#{i} 20"
+    Event.create(date_start: date_start,
+                 date_end: date_end,
+                 location: 'Leafs Coffee',
+                 description: 'bla bla bla bla bla words words words.',
+                 title: event_name).creator(User.find(i))
+  end
+end
+
+user_gen()
+event_gen()
