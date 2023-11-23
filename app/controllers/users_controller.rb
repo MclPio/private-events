a0ea2502bd9a87558a6309e62a8ae4e602015f6d
MclPio <113801014+MclPio@users.noreply.github.com>
2023-11-23 16:18:45 -0500

    Add user authprization for user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :authorize_user!
+
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+
+  def authorize_user!
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to root_path, alert: 'You are not authorized to perform this action'
+    end
   end
 end
